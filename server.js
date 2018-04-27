de2143e664143bf64deefb9be7677c2cde002d08
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-04-27 22:35:41 +0200

    first test

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('First test');
+});
+
+app.listen(3000, function () {
+    console.log('Running on port 3000');
+});
