cb50c1a254bb111a822ddb3060ccf93879514574
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-05-13 14:04:51 +0200

    test for db

--- server.js
+++ server.js
@@ -1,9 +1,48 @@
-var express = require('express');
+var express = require('express')
+var mongoClient = require('mongodb').MongoClient
+var assert = require('assert')
 
-var app = express();
+var url = 'mongodb://localhost:27017/testdb'
 
-app.use(express.static(__dirname + '/pages'));
+var app = express()
+
+app.use(express.static(__dirname + '/pages'))
+
+app.get('/getData', function (req, res, next) {
+  var resultArray = []
+  mongoClient.connect(url, function (err, db) {
+    var cursor = db.collection('testPage').find()
+    cursor.forEach(function (doc, err) {
+      resultArray.push(doc)
+      assert.equal(null, err)
+    }, function () {
+      db.close()
+      res.render('/pages/feedback.html', {
+        item: resultArray
+      })
+    })
+  })
+})
+
+app.post('/insert', function (req, res, next) {
+  var item = {
+    title: req.body.feedback
+  }
+
+  mongoClient.connect(url, function (err, db) {
+    db.collection('testPage').insertOne(item, function (err, result) {
+      console.log('item inserted')
+      db.close()
+    })
+  })
+
+  req.redirect('/pages/feedback.html')
+})
+
+app.post('/update', function (req, res, next) {})
+
+app.post('/delete', function (req, res, next) {})
 
 app.listen(3000, function () {
-    console.log('Running on port 3000');
-});
+  console.log('Running on port 3000')
+})
